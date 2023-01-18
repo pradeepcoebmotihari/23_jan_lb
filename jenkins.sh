5d0027ae3a6ff30a9e2d75acb4e6ed6915d4763b
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2023-01-18 11:11:42 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-for i in `aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --query 'TargetHealthDescriptions[*].Target.Id' --output text`
+for i in `aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/firsttarget/79751f4508ef9c54 --query 'TargetHealthDescriptions[*].Target.Id' --output text`
 do
-aws elbv2 deregister-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --targets Id=$i 
+aws elbv2 deregister-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/firsttarget/79751f4508ef9c54 --targets Id=$i 
 echo "[server]" > /etc/ansible/hosts
 aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> /etc/ansible/hosts
 ansible-playbook -e 'host_key_chaking=False' ansible.yaml --private-key=/home/id_rsa -u ansible --ssh-common-args='-o StrictHostKeyChecking=no'
 sleep 330
-aws elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --targets Id=$i
+aws elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/firsttarget/79751f4508ef9c54 --targets Id=$i
 done
