f150285af19e28be0e360a874c5290fee490b069
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2023-01-18 14:29:33 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ do
 aws elbv2 deregister-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/firsttarget/79751f4508ef9c54 --targets Id=$i 
 echo "[server]" > /etc/ansible/hosts
 aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> /etc/ansible/hosts
-ansible-playbook -e 'host_key_chaking=False' ansible.yaml --private-key=/home/id_rsa -u ansible --ssh-common-args='-o StrictHostKeyChecking=no'
+ansible-playbook -e 'host_key_chaking=False' ansible.yaml --private-key=/home/ranjan.pem -u ranjan --ssh-common-args='-o StrictHostKeyChecking=no'
 sleep 330
 aws elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/firsttarget/79751f4508ef9c54 --targets Id=$i
 done
